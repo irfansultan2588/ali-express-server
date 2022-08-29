0e75eb9fe3a1c688cc5e88dcf7529cac04642601
irfan <irfansultan201@gmail.com>
2022-08-29 18:14:28 +0500

    update

--- server.mjs
+++ server.mjs
@@ -1,10 +1,8 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors';
-
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
 const productSchema = new mongoose.Schema({
-
     productName: String,
     productPrice: Number,
     currencyCode: String,
@@ -14,78 +12,76 @@ const productSchema = new mongoose.Schema({
     shopName: String,
     createdOn: { type: Date, default: Date.now },
 });
-const productModel = mongoose.model('product', productSchema);
+const productModel = mongoose.model("Product", productSchema);
 
 let app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/products", async (req, res) => {
+    let result = await productModel
+        .find({})
+        .exec()
+        .catch((e) => {
+            console.log("error in db: ", e);
+            res.status(500).send({ message: "error in getting all products" });
+            return;
+        });
+
+    res.send({
+        message: "all products success ",
+        data: result,
+    });
+});
 
 app.post("/product", async (req, res) => {
-
     let body = req.body;
 
     if (
-
-        !body.productName
-        || !body.productPrice
-        || !body.currencyCode
-        || !body.numberOfSale
-        || !body.rating
-        || !body.isFreeShipping
-        || !body.shopName
+        !body.productName ||
+        !body.productPrice ||
+        !body.currencyCode ||
+        !body.numberOfSale ||
+        !body.rating ||
+        !body.isFreeShipping ||
+        !body.shopName
     ) {
-
         res.status(400).send({
-            message: `required feild missing all feilds are required:
+            message: `required field missing, all fields are required: 
             productName
             productPrice
             currencyCode
             numberOfSale
             rating
             isFreeShipping
-            shopName`
-
-        })
+            shopName`,
+        });
         return;
-
     }
 
+    let result = await productModel
+        .create({
+            productName: body.productName,
+            productPrice: body.productPrice,
+            currencyCode: body.currencyCode,
+            numberOfSale: body.numberOfSale,
+            rating: body.rating,
+            isFreeShipping: body.isFreeShipping,
+            shopName: body.shopName,
+        })
+        .catch((e) => {
+            console.log("error in db: ", e);
+            res.status(500).send({ message: "db error in saving product" });
+        });
 
-    let result = await productModel.create({
-
-        productName: body.productName,
-        productPrice: body.productPrice,
-        currencyCode: body.currencyCode,
-        numberOfSale: body.numberOfSale,
-        rating: body.rating,
-        isFreeShipping: body.isFreeShipping,
-        shopName: body.shopName,
-
-    }).catch(e => {
-        console.log("error in db", e);
-        res.status(500).send({ message: "db error in saving product" })
-    })
-
-    console.log("result: ", result)
-    res.send({ message: "product is added in database" })
+    console.log("result: ", result);
+    res.send({ message: "product is added in database" });
 });
 
 let PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`app is running on ${PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
