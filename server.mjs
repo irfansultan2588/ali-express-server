46f9baf15c2a0b0cbedeadbffc1212ac7c603c33
irfan <irfansultan201@gmail.com>
2022-08-29 17:47:00 +0500

    complate

---
+++ server.mjs
@@ -0,0 +1,117 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors';
+
+
+const productSchema = new mongoose.Schema({
+
+    productName: String,
+    productPrice: Number,
+    currencyCode: String,
+    numberOfSale: Number,
+    rating: Number,
+    isFreeShipping: Boolean,
+    shopName: String,
+    createdOn: { type: Date, default: Date.now },
+});
+const productModel = mongoose.model('product', productSchema);
+
+let app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.post("/product", async (req, res) => {
+
+    let body = req.body;
+
+    if (
+
+        !body.productName
+        || !body.productPrice
+        || !body.currencyCode
+        || !body.numberOfSale
+        || !body.rating
+        || !body.isFreeShipping
+        || !body.shopName
+    ) {
+
+        res.status(400).send({
+            message: `required feild missing all feilds are required:
+            productName
+            productPrice
+            currencyCode
+            numberOfSale
+            rating
+            isFreeShipping
+            shopName`
+
+        })
+        return;
+
+    }
+
+
+    let result = await productModel.create({
+
+        productName: body.productName,
+        productPrice: body.productPrice,
+        currencyCode: body.currencyCode,
+        numberOfSale: body.numberOfSale,
+        rating: body.rating,
+        isFreeShipping: body.isFreeShipping,
+        shopName: body.shopName,
+
+    }).catch(e => {
+        console.log("error in db", e);
+        res.status(500).send({ message: "db error in saving product" })
+    })
+
+    console.log("result: ", result)
+    res.send({ message: "product is added in database" })
+});
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`app is running on ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+let dbURI = 'mongodb+srv://irfan:irfan@cluster0.jqfzaar.mongodb.net/ProductDataBase?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
